f3c74ece8b4147962226cdbdafdd83b03654cc74
ray park <ray@rayui-MacBookPro.local>
2026-02-06 16:18:53 +0900

    feat: redirect root path to tracking page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { DashboardLayout } from './layouts/DashboardLayout';
-import { Dashboard } from './pages/Dashboard';
 import { TrackingList } from './pages/TrackingList';
 import { TrackingDetail } from './pages/TrackingDetail';
 
@@ -9,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<DashboardLayout />}>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Navigate to="/tracking" replace />} />
           <Route path="/tracking" element={<TrackingList />} />
           <Route path="/tracking/:id" element={<TrackingDetail />} />
           <Route path="*" element={<div className="p-8 text-gray-500">페이지를 찾을 수 없습니다.</div>} />
